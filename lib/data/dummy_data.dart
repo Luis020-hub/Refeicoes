65954631abbf50f95363345d41d2dbd952f74dfd
Luis020-hub <guhmonteiro10@hotmail.com>
2023-02-03 22:45:20 -0300

    03/02/2023
    - Definindo componentes, criando screens e categorias para futuras refeições.

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+// ignore: constant_identifier_names
+const DUMMY_CATEGORIES = [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.white,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Easy & Fast',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c4',
+    title: 'German',
+    color: Colors.black87,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Healthy Food',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.amberAccent,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.indigo,
+  ),
+  Category(
+    id: 'c9',
+    title: 'French',
+    color: Colors.blueAccent,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.orangeAccent,
+  ),
+];
