8915f0b6caf9566e72e03d0e988302d94e9e6230
Luis020-hub <guhmonteiro10@hotmail.com>
2023-02-10 21:10:44 -0300

    10/02/2023
    - Criando a classe Settings.
    - Filtrando tipos de refeições usando a nova classe Settings.

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,20 +1,80 @@
 import 'package:flutter/material.dart';
 import '../components/main_drawer.dart';
+import '../models/settings.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function onChange,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      value: value,
+      onChanged: null,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Settings'),
-      ),
-      drawer: const MainDrawer(),
-      body: const Center(
-        child: Text('Settings!'),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Settings'),
+        ),
+        drawer: const MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                top: 0,
+                bottom: 0,
+              ),
+              child: Text(
+                '',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                _createSwitch(
+                  'Fluten free',
+                  'Only displays gluten-free meals!',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isGlutenFree = value),
+                ),
+                _createSwitch(
+                  'Lactose free',
+                  'Only displays lactose-free meals!',
+                  settings.isLactoseFree,
+                  (value) => setState(() => settings.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegan',
+                  'Only displays vegan meals!',
+                  settings.isVegan,
+                  (value) => setState(() => settings.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetarian',
+                  'Only displays vegetarian meals!',
+                  settings.isVegetarian,
+                  (value) => setState(() => settings.isVegetarian = value),
+                )
+              ],
+            ))
+          ],
+        ));
   }
 }
